180d5fcc7bd750874b751b2e6ffaa32f99ef6c47
RodneyDong <rodneydong156@gmail.com>
2021-09-21 20:55:16 -0400

    first check in

---
+++ rodney/src/com/rodney/myproject/Hello.java
@@ -0,0 +1,14 @@
+package com.rodney.myproject;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		int b = 6;
+		int c = a+b;
+		System.out.println(c);
+		System.out.print
+	}
+
+}
